900db6d8afd1bd30852377ecd1ee8fd9e39e4b8c
Marketa Konecna <konecnamarketa98@seznam.cz>
2024-06-29 17:57:08 +0200

    vezre1

---
+++ library/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+from django.http import HttpResponse
+import os
+
+
+def index(request):
+    return render (request, "index.html")
