6dd9cbca63aac208be0977a2410765432580b15f
Marketa Konecna <konecnamarketa98@seznam.cz>
2024-06-30 11:15:05 +0200

    Logit and Register

--- library/urls.py
+++ library/urls.py
@@ -4,4 +4,7 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("login", views.login_view, name="login"),
+    path("register", views.register, name="register"),
+    path("logout", views.logout_view, name="logout")
 ]
