de320b5374bdab1921ce43fcc0e4ca4eaf8700aa
Marketa Konecna <konecnamarketa98@seznam.cz>
2024-08-06 21:34:54 +0200

    upload file

--- library/urls.py
+++ library/urls.py
@@ -4,9 +4,11 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("best_sellers", views.best_sellers, name="best_sellers"),
     path("login", views.login_view, name="login"),
     path("register", views.register, name="register"),
     path("logout", views.logout_view, name="logout"),
     # path("more_info_a_book/<str:category>", views.more_info_a_book, name="more_info_a_book"),
-    path("more_info_a_book/<str:category>/<str:number>", views.more_info_a_book, name="more_info_a_book")
+    path("more_info_a_book/<str:category>/<str:number>", views.more_info_a_book, name="more_info_a_book"),
+    path('upload/', views.upload_file, name='upload_file')
 ]
