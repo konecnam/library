dc5e9f4f3a9cbe3a3f3b0426dcdd770a1cb5fa20
Marketa Konecna <konecnamarketa98@seznam.cz>
2024-10-24 19:29:37 +0200

    test done

--- library/tests.py
+++ library/tests.py
@@ -5,6 +5,8 @@ from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium.webdriver.common.by import By
 from selenium.webdriver.firefox.webdriver import WebDriver
 from .models import User, UploadedBook, MyBook
+from selenium.webdriver.support.ui import Select
+import time
 
 class Book_test(TestCase):
     def test_best_book_I(self):
@@ -206,4 +208,280 @@ class MyUserIn(StaticLiveServerTestCase):
         self.selenium.find_element(By.XPATH, '//input[@value="Register"]').click()
         title = self.selenium.find_element(By.CLASS_NAME, "title_bestsellers")
         self.assertEqual(title.text, "Best sellers")
+
+    def test_add_book(self):
+        self.login()
+        link = self.selenium.find_element(By.XPATH,'/html/body/div/div/header/div[2]/nav/a[2]')
+        link.click()
+        add_book = self.selenium.find_element(By.XPATH, '/html/body/div/div/main/div/div[1]/button/span')
+        add_book.click()
+
+        book_title = self.selenium.find_element(By.NAME, "book_title")
+        book_title.send_keys("Zacit znovu")
+        author = self.selenium.find_element(By.NAME, "author")
+        author.send_keys("Mona Kasten")
+        book_description = self.selenium.find_element(By.NAME, "book_description")
+        book_description.send_keys("Romantic books")
+        image = self.selenium.find_element(By.NAME, "image")
+        image.send_keys("https://cdn.palmknihy.cz/prod-media-assets/01G6JVHKM658H6P5Z3ZN8HWC9W/0302400-20.jpg?v=ed3de541")
+
+        category_dropdown = Select(self.selenium.find_element(By.NAME, "category"))
+        category_dropdown.select_by_visible_text("Romantic") 
+        save = self.selenium.find_element(By.XPATH, '/html/body/div/div/main/div/form[1]/input[2]')
+        save.click()
+        title = self.selenium.find_element(By.XPATH, '/html/body/div/div/main/div/div[2]/div[1]/div[2]/div/h5[1]')
+        self.assertEqual(title.text, "Zacit znovu")
+        author = self.selenium.find_element(By.XPATH, '/html/body/div/div/main/div/div[2]/div[1]/div[2]/div/h5[2]')
+        self.assertEqual(author.text, "Mona Kasten")
+        book_description = self.selenium.find_element(By.XPATH, '/html/body/div/div/main/div/div[2]/div[1]/div[2]/div/p[1]')
+        self.assertEqual(book_description.text, "Book description: Romantic books")
+        image = self.selenium.find_element(By.XPATH, '/html/body/div/div/main/div/div[2]/div[1]/div[1]/div')
+        background_image = image.value_of_css_property('background')
+        background_image_url = background_image.split('"')[1]
+        self.assertEqual(background_image_url, "https://cdn.palmknihy.cz/prod-media-assets/01G6JVHKM658H6P5Z3ZN8HWC9W/0302400-20.jpg?v=ed3de541")
+        
+        self.assertEqual(book_description.text, "Book description: Romantic books")
+        category = self.selenium.find_element(By.XPATH, '/html/body/div/div/main/div/div[2]/div[1]/div[2]/div/p[2]')
+        self.assertEqual(category.text, "Category: Romantic")
+
+    def test_add_book_none(self):
+        self.login()
+        link = self.selenium.find_element(By.XPATH,'/html/body/div/div/header/div[2]/nav/a[2]')
+        link.click()
+        add_book = self.selenium.find_element(By.XPATH, '/html/body/div/div/main/div/div[1]/button/span')
+        add_book.click()
+        category_dropdown = Select(self.selenium.find_element(By.NAME, "category"))
+        category_dropdown.select_by_visible_text("Romantic") 
+        save = self.selenium.find_element(By.XPATH, '/html/body/div/div/main/div/form[1]/input[2]')
+        save.click()
+        text = self.selenium.find_element(By.XPATH, '/html/body/div/div/main/div/div[1]/h5')
+        self.assertEqual(text.text, "Not all fields are filled in!")
+
+
+    def test_add_book_none_image(self):
+        self.login()
+        link = self.selenium.find_element(By.XPATH,'/html/body/div/div/header/div[2]/nav/a[2]')
+        link.click()
+        add_book = self.selenium.find_element(By.XPATH, '/html/body/div/div/main/div/div[1]/button/span')
+        add_book.click()
+        book_title = self.selenium.find_element(By.NAME, "book_title")
+        book_title.send_keys("Stále znovu")
+        author = self.selenium.find_element(By.NAME, "author")
+        author.send_keys("Mona Kasten")
+        book_description = self.selenium.find_element(By.NAME, "book_description")
+        book_description.send_keys("Romantic books")
+        save = self.selenium.find_element(By.XPATH, '/html/body/div/div/main/div/form[1]/input[2]')
+        save.click()
+        text = self.selenium.find_element(By.XPATH, '/html/body/div/div/main/div/div[1]/h5')
+        self.assertEqual(text.text, "Not all fields are filled in!")
+
+    def test_edit(self):
+        user = User.objects.get(username='hroch')
+        all_inf_my = MyBook (book_title='Sexy pilot', author='XXX', book_description='eroticka kniha', image='xxx', category='Romantic', created_by=user)
+        all_inf_my.save()
+        self.login()
+        link = self.selenium.find_element(By.XPATH,'/html/body/div/div/header/div[2]/nav/a[2]')
+        link.click()
+        edit = self.selenium.find_element(By.XPATH, '/html/body/div/div/main/div/div[2]/div[1]/div[2]/div/div/button')
+        edit.click()
+
+        title = self.selenium.find_element(By.XPATH, '//html/body/div/div/main/div/div[2]/div[1]/div[2]/form[1]/div[1]/input')
+        title.clear()
+        title.send_keys("Sexy profesor")
+        author = self.selenium.find_element(By.XPATH, '/html/body/div/div/main/div/div[2]/div[1]/div[2]/form[1]/div[2]/input')
+        author.clear()
+        author.send_keys("ZZZ ")
+        category_dropdown = Select(self.selenium.find_element(By.NAME, "category"))
+        category_dropdown.select_by_visible_text("Other")
+        submit = self.selenium.find_element(By.XPATH, '/html/body/div/div/main/div/div/div[1]/div[2]/form[1]/button[1]')
+        submit.click()
+
+        title = self.selenium.find_element(By.XPATH, '/html/body/div/div/main/div/div/div[1]/div[2]/div/h5[1]')
+        self.assertEqual(title.text, "Sexy profesor")
+        author = self.selenium.find_element(By.XPATH, '/html/body/div/div/main/div/div/div[1]/div[2]/div/h5[2]')
+        self.assertEqual(author.text, "ZZZ")
+        book_description = self.selenium.find_element(By.XPATH, '/html/body/div/div/main/div/div/div[1]/div[2]/div/p[1]')
+        self.assertEqual(book_description.text, "Book description: eroticka kniha")
+        image = self.selenium.find_element(By.XPATH, '/html/body/div/div/main/div/div[2]/div[1]/div[1]/div')
+        background_image = image.value_of_css_property('background')
+        background_image_url = background_image.split('"')[1]
+        self.assertEqual(background_image_url, "xxx")
+        category = self.selenium.find_element(By.XPATH, '//html/body/div/div/main/div/div/div[1]/div[2]/div/p[2]')
+        self.assertEqual(category.text, "Category: Other")
+
+    def test_edit_cancel(self):
+        user = User.objects.get(username='hroch')
+        all_inf_my = MyBook (book_title='Sexy pilot', author='XXX', book_description='eroticka kniha', image='xxx', category='Romantic', created_by=user)
+        all_inf_my.save()
+        self.login()
+        link = self.selenium.find_element(By.XPATH,'/html/body/div/div/header/div[2]/nav/a[2]')
+        link.click()
+        edit = self.selenium.find_element(By.XPATH, '/html/body/div/div/main/div/div[2]/div[1]/div[2]/div/div/button')
+        edit.click()
+
+        title = self.selenium.find_element(By.XPATH, '//html/body/div/div/main/div/div[2]/div[1]/div[2]/form[1]/div[1]/input')
+        title.clear()
+        title.send_keys("Sexy profesor")
+        author = self.selenium.find_element(By.XPATH, '/html/body/div/div/main/div/div[2]/div[1]/div[2]/form[1]/div[2]/input')
+        author.clear()
+        author.send_keys("ZZZ ")
+        book_description = self.selenium.find_element(By.XPATH, '/html/body/div/div/main/div/div[2]/div[1]/div[2]/form[1]/div[3]/input')
+        category_dropdown = Select(self.selenium.find_element(By.NAME, "category"))
+        category_dropdown.select_by_visible_text("Other")
+        cancel = self.selenium.find_element(By.XPATH, '//html/body/div/div/main/div/div[2]/div[1]/div[2]/form[1]/button[2]')
+        cancel.click()
+        title = self.selenium.find_element(By.XPATH, '/html/body/div/div/main/div/div/div[1]/div[2]/div/h5[1]')
+        self.assertEqual(title.text, "Sexy pilot")
+        author = self.selenium.find_element(By.XPATH, '/html/body/div/div/main/div/div/div[1]/div[2]/div/h5[2]')
+        self.assertEqual(author.text, "XXX")
+        book_description = self.selenium.find_element(By.XPATH, '/html/body/div/div/main/div/div/div[1]/div[2]/div/p[1]')
+        self.assertEqual(book_description.text, "Book description: eroticka kniha")
+        category = self.selenium.find_element(By.XPATH, '//html/body/div/div/main/div/div/div[1]/div[2]/div/p[2]')
+        self.assertEqual(category.text, "Category: Romantic")
+    
+    def test_delete(self):
+        user = User.objects.get(username='hroch')
+        all_inf_my = MyBook (book_title='YYY', author='XXX', book_description='ZZZ', image='xxx', category='Romantic', created_by=user)
+        all_inf_my.save()
+        self.login()
+        link = self.selenium.find_element(By.XPATH,'/html/body/div/div/header/div[2]/nav/a[2]')
+        link.click()
+        title = self.selenium.find_element(By.XPATH, '/html/body/div/div/main/div/div[2]/div[1]/div[2]/div/h5[1]')
+        self.assertEqual(title.text, "YYY")
+        author = self.selenium.find_element(By.XPATH, '/html/body/div/div/main/div/div[2]/div[1]/div[2]/div/h5[2]')
+        self.assertEqual(author.text, "XXX")
+        book_description = self.selenium.find_element(By.XPATH, '/html/body/div/div/main/div/div[2]/div[1]/div[2]/div/p[1]')
+        self.assertEqual(book_description.text, "Book description: ZZZ")
+        delete = self.selenium.find_element(By.XPATH, '/html/body/div/div/main/div/div[2]/div[1]/div[2]/div/div/form/button/i')
+        delete.click()
+        with self.assertRaises(MyBook.DoesNotExist):
+            MyBook.objects.get(book_title='YYY', author='XXX', book_description='ZZZ')
+    
+    def test_stars(self):
+        user = User.objects.get(username='hroch')
+        all_inf_my = MyBook (book_title='YYY', author='XXX', book_description='ZZZ', image='xxx', category='Romantic', created_by=user)
+        all_inf_my.save()
+        self.login()
+        link = self.selenium.find_element(By.XPATH,'/html/body/div/div/header/div[2]/nav/a[2]')
+        link.click()
+        stars = self.selenium.find_element(By. XPATH, '/html/body/div/div/main/div/div[2]/div[1]/div[2]/form/label[3]/span[3]')
+        stars.click()
+        new_book = MyBook.objects.get(book_title='YYY', author='XXX', book_description='ZZZ')
+        self.assertEqual(new_book.rating, 3)
+
+    def test_page(self):
+        user = User.objects.get(username='hroch')
+        for i in range(1, 10):  
+            MyBook(
+                book_title=f'book_title {i}',
+                author=f'author {i}',
+                book_description=f'book_description {i}',
+                image=f'image {i}',
+                category='Romantic',
+                created_by=user
+                ).save()
+            time.sleep(1)
+        self.login()
+        link = self.selenium.find_element(By.XPATH,'/html/body/div/div/header/div[2]/nav/a[2]')
+        link.click()
+        first = self.selenium.find_element(By.XPATH, '/html/body/div/div/main/div/div[2]/div[1]/div[2]/div/h5[1]')
+        self.assertEqual(first.text, "book_title 9")
+        two = self.selenium.find_element(By.XPATH, '/html/body/div/div/main/div/div[2]/div[8]/div[2]/div/h5[1]')
+        self.assertEqual(two.text, "book_title 2")
+        page_2 = self.selenium.find_element(By.XPATH, '/html/body/div/div/main/div/div[2]/nav/ul/li[2]/a')
+        page_2.click()
+        one = self.selenium.find_element(By.XPATH,'/html/body/div/div/main/div/div[2]/div[1]/div[2]/div/h5[1]')
+        self.assertEqual(one.text, "book_title 1")
+    
+    def test_page_next(self):
+        user = User.objects.get(username='hroch')
+        for i in range(1, 22):  
+            MyBook(
+                book_title=f'book_title {i}',
+                author=f'author {i}',
+                book_description=f'book_description {i}',
+                image=f'image {i}',
+                category='Romantic',
+                created_by=user
+                ).save()
+            time.sleep(0.1)
+        self.login()
+        link = self.selenium.find_element(By.XPATH,'/html/body/div/div/header/div[2]/nav/a[2]')
+        link.click()
+        page_finish = self.selenium.find_element(By.XPATH, '/html/body/div/div/main/div/div[2]/nav/ul/a')
+        page_finish.click()
+        one = self.selenium.find_element(By.XPATH,'/html/body/div/div/main/div/div[2]/div[8]/div[2]/div/h5[1]')
+        self.assertEqual(one.text, "book_title 6")
+        back = self.selenium.find_element(By.XPATH, '/html/body/div/div/main/div/div[2]/nav/ul/li[1]/a')
+        back.click()
+        book_14 = self.selenium.find_element(By.XPATH, '/html/body/div/div/main/div/div[2]/div[8]/div[2]/div/h5[1]')
+        self.assertEqual(book_14.text, "book_title 14")
+    
+    def test_search(self):
+        user = User.objects.get(username='hroch')
+        first_book = MyBook (book_title='miluji tě', author='Mona Kasten', book_description='ZZZ', image='xxx', category='Romantic', created_by=user)
+        first_book.save()
+        second_book = MyBook (book_title='auto', author='Julie Caplinova', book_description='BBB', image='AAA', category='Romantic', created_by=user)
+        second_book.save()
+        self.login()
+        link = self.selenium.find_element(By.XPATH,'/html/body/div/div/header/div[2]/nav/a[2]')
+        link.click()
+        search = self.selenium.find_element(By.XPATH, '/html/body/div/div/main/div/form/div/div/input')
+        search.send_keys('miluji')
+        button_search = self.selenium.find_element(By.XPATH, '/html/body/div/div/main/div/form/button')
+        button_search.click()
+        result = self.selenium.find_element(By.XPATH, '/html/body/div/div/main/div/div[2]/div/div[2]/div/h5[1]')
+        self.assertEqual('miluji tě', result.text)
+
+    def test_search_2(self):
+        user = User.objects.get(username='hroch')
+        first_book = MyBook (book_title='miluji tě', author='Mona Kasten', book_description='ZZZ', image='xxx', category='Romantic', created_by=user)
+        first_book.save()
+        second_book = MyBook (book_title='auto', author='Miluj Caplinova', book_description='BBB', image='AAA', category='Romantic', created_by=user)
+        second_book.save()
+        self.login()
+        link = self.selenium.find_element(By.XPATH,'/html/body/div/div/header/div[2]/nav/a[2]')
+        link.click()
+        search = self.selenium.find_element(By.XPATH, '/html/body/div/div/main/div/form/div/div/input')
+        search.send_keys('milu')
+        button_search = self.selenium.find_element(By.XPATH, '/html/body/div/div/main/div/form/button')
+        button_search.click()
+        result_1 = self.selenium.find_element(By.XPATH, '/html/body/div/div/main/div/div[2]/div/div[2]/div/h5[1]')
+        self.assertEqual('miluji tě', result_1.text)
+        result_2 = self.selenium.find_element(By.XPATH, '/html/body/div/div/main/div/div[2]/div[2]/div[2]/div/h5[2]')
+        self.assertEqual('Miluj Caplinova', result_2.text)
+
+    def test_search_3(self):
+        user = User.objects.get(username='hroch')
+        first_book = MyBook (book_title='miluji tě', author='Mona Kasten', book_description='ZZZ', image='xxx', category='Romantic', created_by=user)
+        first_book.save()
+        second_book = MyBook (book_title='auto', author='Julie Caplinova', book_description='BBB', image='AAA', category='Romantic', created_by=user)
+        second_book.save()
+        self.login()
+        link = self.selenium.find_element(By.XPATH,'/html/body/div/div/header/div[2]/nav/a[2]')
+        link.click()
+        search = self.selenium.find_element(By.XPATH, '/html/body/div/div/main/div/form/div/div/input')
+        search.send_keys('bla')
+        button_search = self.selenium.find_element(By.XPATH, '/html/body/div/div/main/div/form/button')
+        button_search.click()
+        result = self.selenium.find_element(By.XPATH, '/html/body/div/div/main/div/div[2]/h5')
+        self.assertEqual('No books saved yet.', result.text)
+        
+        
+
+
+        
+
+        
+
+        
+        
+     
+        
+       
+
+        
+
+       
+
         
